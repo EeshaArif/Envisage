c8ea179d24db9a89895ea8ea675dd0a54240ed64
EeshaArif <eeshaarif@gmail.com>
2021-01-01 15:16:09 +0500

    :perf(webService):Add error handling code

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -1,36 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { Message } from './interfaces/message';
 @Injectable({
   providedIn: 'root',
 })
 export class WebService {
   BASE_URL = 'http://localhost:3000';
-  messages: any = [];
+  messages: Message[] = [];
   constructor(private http: HttpClient, private sb: MatSnackBar) {
     this.getMessages();
   }
-  async getMessages() {
-    var response: any = this.http.get(`${this.BASE_URL}/api/message`).pipe(
-      catchError((err) => {
-        this.sb.open('Unable to get messages', 'close', { duration: 5000 });
-        return throwError('Cannot Get Messages');
-      })
-    );
-    response.subscribe((messages: any) => (this.messages = messages));
+  getMessages(): void {
+    this.http
+      .get<Message[]>(`${this.BASE_URL}/api/message`)
+      .pipe(
+        catchError((err) => {
+          this.sb.open('Unable to get messages', 'close', { duration: 5000 });
+          return throwError('Cannot Get Messages');
+        })
+      )
+      .subscribe((messages) => {
+        this.messages = messages;
+      });
   }
-  async postMessage(message: Message) {
-    var response = this.http.post(`${this.BASE_URL}/api/message`, message);
-    response.subscribe();
+  postMessage(message: Message): void {
+    this.http
+      .post(`${this.BASE_URL}/api/message`, message)
+      .pipe(
+        catchError((err) => {
+          this.sb.open('Unable to post message', 'close', { duration: 5000 });
+          return throwError('Cannot Post Message');
+        })
+      )
+      .subscribe();
     this.messages.push(message);
   }
-}
-interface Message {
-  text: string;
-  owner: string;
-}
-interface MessagesResponse {
-  messages: Message[];
 }
