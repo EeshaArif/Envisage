60a5e41082228a1fa6560d46b982d2e068694fa7
EeshaArif <eeshaarif@gmail.com>
2021-01-02 14:23:58 +0500

    :ref(auth):Add Navigation

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,14 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   BASE_URL: string = environment.authUrl;
+  NAME_KEY = 'name';
+  TOKEN_KEY = 'token';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
+  get name() {
+    return localStorage.getItem(this.NAME_KEY);
+  }
+  get isAuthenticated() {
+    return !!localStorage.getItem(this.TOKEN_KEY);
+  }
   register(user: any): void {
     delete user.confirmPassword;
     this.http
@@ -17,7 +26,12 @@ export class AuthService {
         user
       )
       .subscribe((res) => {
-        localStorage.setItem('token', res.token);
+        if (!res.token) {
+          return;
+        }
+        localStorage.setItem(this.TOKEN_KEY, res.token);
+        localStorage.setItem(this.NAME_KEY, res.firstName);
+        this.router.navigate(['/']);
       });
   }
 }
